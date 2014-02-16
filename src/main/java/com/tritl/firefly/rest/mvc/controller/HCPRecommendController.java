1ff9d7b5659f6c8e16c3fa62727f748b0bbabfee
kganesh <ganesh.kolekar@gmail.com>
2014-02-16 12:12:50 -0800

    changes for hcp recommend

--- src/main/java/com/tritl/firefly/rest/mvc/controller/HCPRecommendController.java
+++ src/main/java/com/tritl/firefly/rest/mvc/controller/HCPRecommendController.java
@@ -3,6 +3,7 @@ package com.tritl.firefly.rest.mvc.controller;
 import java.util.List;
 import java.util.ArrayList;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,22 +12,30 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.tritl.firefly.model.Doctor;
+import com.tritl.firefly.model.InsuranceProvider;
+import com.tritl.firefly.service.FireflyDataServices;
 
 @Controller
 @RequestMapping("/hcprecommend")
 public class HCPRecommendController {
+	
+	private FireflyDataServices fireflyDataService;
+
+	@Autowired
+	public void setFireflyDataService(FireflyDataServices fireflyDataService) {
+		this.fireflyDataService = fireflyDataService;
+	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public @ResponseBody
-	List<Doctor> getShopInJSON(@RequestParam(value = "dob") String dob,
+	List<InsuranceProvider> getHCPRecommendList(@RequestParam(value = "dob") String dob,
 			@RequestParam(value = "zip") String zip,
 			@RequestParam(value = "provider") String provider,
 			@RequestParam(value = "diseases") String[] diseases) {
 
 		
-
-		return null;
-
+		return fireflyDataService.getHCPRecommendList(dob, zip, provider, diseases);
+		
 	}
 
 }
